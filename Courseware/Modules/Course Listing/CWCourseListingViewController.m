1cca3a5ad32fe92a8f9e39784d9a64da34a254e6
Leo Angelo Quigao <leoangelo@fbmsoftware.com>
2012-09-15 22:24:22 +0800

    Improved course listing look-and-feel
    Signed-off-by: Leo Angelo Quigao <leoangelo@fbmsoftware.com>

--- Courseware/Modules/Course Listing/CWCourseListingViewController.m
+++ Courseware/Modules/Course Listing/CWCourseListingViewController.m
@@ -69,18 +69,21 @@
 	return self.model.getItemList.count;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	CWCourseItem *itemAtI = [self.model.getItemList objectAtIndex:indexPath.row];
+	return [self cellHeightForDepth:itemAtI.depth];
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-	if (!cell) {
-		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"] autorelease];
-		[cell setIndentationWidth:30];
-	}
 	CWCourseItem *itemAtI = [self.model.getItemList objectAtIndex:indexPath.row];
-	cell.textLabel.text = [itemAtI.data objectForKey:kCourseItemTitle];
 	
-	[cell setIndentationLevel:itemAtI.depth];
-	
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[self cellIdentifierForDepth:itemAtI.depth]];
+	if (!cell) {
+		cell = [self cellTemplateForDepth:itemAtI.depth];
+	}
+	[self reconfigureCell:cell withCourseItem:itemAtI];
 	return cell;
 }
 
@@ -106,6 +109,63 @@
 	if (indexForItem != NSNotFound) {
 		[self.listView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:indexForItem inSection:0] atScrollPosition:UITableViewScrollPositionTop animated:YES];
 	}
+}
+
+#pragma mark - Configuring Cell Appearance
+
+- (CGFloat)cellHeightForDepth:(NSInteger)depth
+{
+	switch (depth) {
+		case 0: return 70.f;
+		case 1: return 70.0f;
+		case 2: return 65;
+	}
+	return 50.0f;
+}
+
+- (NSString *)cellIdentifierForDepth:(NSInteger)depth
+{
+	return [NSString stringWithFormat:@"course-list-depth-%i", depth];
+}
+
+- (UITableViewCell *)cellTemplateForDepth:(NSInteger)depth
+{
+	UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:[self cellIdentifierForDepth:depth]];
+	
+	NSString *fontName = nil;
+	CGFloat fontSize = 0;
+	// UITableViewCellAccessoryType accessoryType;
+	
+	if (depth <= 1) {
+		fontName = @"FuturaLT-Heavy";
+	} else if (depth <= 2) {
+		fontName = @"FuturaLT";
+	} else {
+		fontName = @"FuturaLT-Light";
+	}
+	
+	switch (depth) {
+		case 0: fontSize = 23; break;
+		case 1: fontSize = 21; break;
+		case 2: fontSize = 20; break;
+		default: fontSize = 18; break;
+	}
+	
+	cell.textLabel.font = [UIFont fontWithName:fontName size:fontSize];
+	cell.detailTextLabel.font = [UIFont fontWithName:fontName size:fontSize - 3];
+	
+	return [cell autorelease];
+}
+
+- (void)reconfigureCell:(UITableViewCell *)cell withCourseItem:(CWCourseItem *)item
+{
+	cell.textLabel.text = [item.data objectForKey:kCourseItemTitle];
+	
+	if ([item.data objectForKey:kCourseItemDescription]) {
+		cell.detailTextLabel.text = [item.data objectForKey:kCourseItemDescription];
+	}
+	
+	[cell setIndentationLevel:item.depth];
 }
 
 @end
